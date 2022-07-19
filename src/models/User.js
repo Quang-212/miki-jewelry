0d17f06236e880c1f12dd16ba83240cac8acd3da
Dat266 <datdev266@gmail.com>
2022-07-19 08:36:16 +0700

    add UserSchema

--- src/models/User.js
+++ src/models/User.js
@@ -2,14 +2,30 @@ import { Schema, models, model } from 'mongoose';
 
 const UserSchema = new Schema(
   {
-    name: { type: String },
-    email: String,
-    //more field here
+    email: {
+      type: String,
+      unque: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      min: 8,
+    },
+    confirmpassword: {
+      type: String,
+      require: true,
+      min: 8,
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   {
     timestamps: true,
-  }
+  },
 );
 
 export default models.User || model('User', UserSchema);
