e4c7577c06d1e20d394f64b3092133343890a11f
anIcedAntFA <anicedfaant@gmail.com>
2022-07-24 15:58:20 +0700

    Update file global.css, tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,44 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        primary: '#251C17',
-        secondary: '',
-      },
       fontFamily: {
         primary: ['Montserrat'],
         secondary: ['Playfair Display'],
+      },
+      fontSize: {
+        '32-px': '32px',
+      },
+      colors: {
+        primary: '#251C17',
+        secondary: '#6E5544',
       },
       maxWidth: {
         '1440-px': '1440px',
       },
       borderRadius: {
-        '8-px': '8x',
-        secondary: '16x',
+        none: '0',
+        sm: '0.125rem',
+        md: '0.5rem',
+        lg: '1rem',
       },
       spacing: {
-        '13-px': '13px',
+        '120-px': '120px',
+        '146-px': '146px',
+        '342-px': '342px',
+      },
+      padding: {
+        '8-px': '8px',
+        '12-px': '12px',
+        '24-px': '24px',
+        '32-px': '32px',
+        '46-px': '46px',
+        '152-px': '152px',
+      },
+      lineHeight: {
+        '48-px': '48px',
+        '58-px': '56px',
+        '106-px': '106px',
+        '128-px': '128px',
       },
     },
   },
