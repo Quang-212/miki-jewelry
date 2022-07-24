d1ea569af1e54ded0de4d077bdeaa39297208f67
anIcedAntFA <anicedfaant@gmail.com>
2022-07-24 16:18:27 +0700

    add folder constants

---
+++ src/constants/images.jsx
@@ -0,0 +1,21 @@
+import homeAbout from 'public/static/images/home-about.png';
+import homeBestSellerProduct from 'public/static/images/home-best-seller-product.png';
+import homeCategoryBracelet from 'public/static/images/home-category-bracelet.png';
+import homeCategoryEarring from 'public/static/images/home-category-earring.png';
+import homeCategoryNecklace from 'public/static/images/home-category-necklace.png';
+import homeCategoryRing from 'public/static/images/home-category-ring.png';
+import homeCategoryWatch from 'public/static/images/home-category-watch.png';
+import homeHero from 'public/static/images/home-hero.png';
+import homeLatestCollection from 'public/static/images/home-latest-collection.png';
+
+export default {
+  homeHero,
+  homeAbout,
+  homeBestSellerProduct,
+  homeLatestCollection,
+  homeCategoryRing,
+  homeCategoryWatch,
+  homeCategoryBracelet,
+  homeCategoryNecklace,
+  homeCategoryEarring,
+};
