b96d2784acbc814486806223963debdd4064c3c7
Dat266 <datdev266@gmail.com>
2022-08-18 09:07:28 +0700

    fix

--- src/pages/api/products/update.js
+++ src/pages/api/products/update.js
@@ -3,17 +3,17 @@ import Product from 'src/models/Product';
 
 async function updateProduct(req, res) {
   await dbConnect();
-  const { id } = req.query;
-  console.log(id);
   const { method } = req;
+  const { _id } = req.body;
+  console.log(_id);
+
   if (method == 'POST') {
-    await Product.findOneAndUpdate(id, {
-      ...req.body,
-    });
+    const data = await Product.findOneAndUpdate(_id);
 
     return res.status(200).json({
       message: 'Update thành công',
       code: 200,
+      data,
     });
   }
 }
