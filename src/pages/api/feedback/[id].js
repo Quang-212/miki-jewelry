a3dabbb15d8c3420f94561da11193ba514994e2b
Dat266 <datdev266@gmail.com>
2022-08-19 19:35:22 +0700

    replace feedback

--- src/pages/api/feedback/[id].js
+++ src/pages/api/feedback/[id].js
@@ -5,7 +5,7 @@ import dbConnect from 'src/utils/dbConnect';
 async function handlerRating(req, res) {
   await dbConnect();
   const { product_id } = req.query;
-  const getProductId = await Rating.aggregate({ product_id: req.query.product_id });
+  const getProductId = await Rating.aggregate({ product_id });
   return res.status(200).json({
     message: 'thành công',
     code: 200,
