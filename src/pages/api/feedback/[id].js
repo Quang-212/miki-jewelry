37bd33f4de80468492b73e7e6c0b68064d9ca886
Dat266 <datdev266@gmail.com>
2022-08-19 19:33:20 +0700

    add file feedback/[id].js

--- src/pages/api/feedback/[id].js
+++ src/pages/api/feedback/[id].js
@@ -4,13 +4,8 @@ import dbConnect from 'src/utils/dbConnect';
 
 async function handlerRating(req, res) {
   await dbConnect();
-  const product_id = mongoose.Types.ObjectId(req.query.product_id);
-  const getProductId = await Rating.aggregate([
-    {
-      $match: { product_id },
-      $group: { _id: '$user_id', count: { $sum: 1 } },
-    },
-  ]);
+  const { product_id } = req.query;
+  const getProductId = await Rating.aggregate({ product_id: req.query.product_id });
   return res.status(200).json({
     message: 'thành công',
     code: 200,
