4b63265dccd8d9e39edd6f23c754142bb28dcfa3
quang.nv212 <95917117+Quang-212@users.noreply.github.com>
2022-09-29 00:20:39 +0700

    Remove unnecessary switch/case.

--- src/components/RatingStar/PassiveRatingStar/PassiveRatingStar.jsx
+++ src/components/RatingStar/PassiveRatingStar/PassiveRatingStar.jsx
@@ -6,27 +6,10 @@ export default function PassiveRatingStar({
   width,
   color = { filled: '', unfilled: '' },
 }) {
-  const getColor = (index) => {
-    switch (star) {
-      case 5:
-        return color.filled;
-      case 4:
-        return index === 5 ? color.unfilled : color.filled;
-      case 3:
-        return index === 4 || index === 5 ? color.unfilled : color.filled;
-      case 2:
-        return index === 3 || index === 4 || index === 5 ? color.unfilled : color.filled;
-      default:
-        return index === 2 || index === 3 || index === 4 || index === 5
-          ? color.unfilled
-          : color.filled;
-    }
-  };
   return [...Array(count)].map((_, index) => {
-    index += 1;
     return (
       <li key={index}>
-        <RatingStarIcon width={width} className={getColor(index)} />
+        <RatingStarIcon width={width} className={index < star ? color.filled : color.unfilled} />
       </li>
     );
   });
--- src/container/product-details/tabs/tab-reviews/RatingStarsList.jsx
+++ src/container/product-details/tabs/tab-reviews/RatingStarsList.jsx
@@ -4,28 +4,6 @@ import { PassiveRatingStar } from 'src/components/RatingStar';
 const TOTAL_STARS = 5;
 
 export default function RatingStarsListPreview({ data }) {
-  const generateRatingStars = (ratingStar) => {
-    let star = 5;
-    switch (ratingStar) {
-      case 5:
-        star = 5;
-        break;
-      case 4:
-        star = 4;
-        break;
-      case 3:
-        star = 3;
-        break;
-      case 2:
-        star = 2;
-        break;
-      default:
-        star = 1;
-        break;
-    }
-    return star;
-  };
-
   return (
     <ul className="col-span-12 max-w-[242px] mt-2">
       {data.map((rating) => {
@@ -34,7 +12,7 @@ export default function RatingStarsListPreview({ data }) {
             <ul className="flex gap-1">
               <PassiveRatingStar
                 count={TOTAL_STARS}
-                star={generateRatingStars(rating.star)}
+                star={rating.star}
                 width="14"
                 color={{ filled: 'text-active-star', unfilled: 'text-normal-star' }}
               />
