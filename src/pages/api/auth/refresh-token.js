94a95c66c1947292d3ba6ae026c9a13938441b79
quang.nv212 <95917117+Quang-212@users.noreply.github.com>
2022-10-01 01:45:51 +0700

    Fix setHeader after response

--- src/pages/api/auth/refresh-token.js
+++ src/pages/api/auth/refresh-token.js
@@ -1,14 +1,14 @@
 import jwt from 'jsonwebtoken';
-import RefreshToken from 'src/models/RefreshToken';
 import dbConnect from 'src/utils/dbConnect';
+import RefreshToken from 'src/models/RefreshToken';
 import { generateAccessToken } from 'src/utils/generateToken';
 import { REFRESH_TOKEN_KEY } from 'src/pages/api/constant';
 
 async function handlerRefreshToken(req, res) {
-  await dbConnect();
   const { method } = req;
 
   try {
+    await dbConnect();
     switch (method) {
       case 'POST':
         const refreshTokenCookie = req.cookies.refreshToken;
@@ -19,24 +19,25 @@ async function handlerRefreshToken(req, res) {
 
         if (isRefreshToken) {
           jwt.verify(refreshTokenCookie, REFRESH_TOKEN_KEY, (err, payload) => {
-            if (err)
+            if (err) {
               return res.status(401).json({
                 message: 'Token expired',
                 code: 401,
               });
+            }
             const newAccessToken = generateAccessToken(payload);
             return res.status(201).json({
-              message: ' Tạo mới token thành công ',
+              message: 'OK',
               code: 201,
               accessToken: newAccessToken,
             });
           });
+        } else {
+          return res.status(401).json({
+            message: 'Token expired',
+            code: 401,
+          });
         }
-
-        return res.status(401).json({
-          message: 'Token expired',
-          code: 401,
-        });
 
       default:
         return res.status(400).json({
