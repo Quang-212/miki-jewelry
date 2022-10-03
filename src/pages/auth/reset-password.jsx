4b6713e7f6f476164a46ac932d38187bea57569f
quang.nv212 <95917117+Quang-212@users.noreply.github.com>
2022-10-03 15:25:34 +0700

    Add import guard in reset-password

--- src/pages/auth/reset-password.jsx
+++ src/pages/auth/reset-password.jsx
@@ -1,6 +1,6 @@
 import Page from 'src/components/Page';
 import { ResetPasswordForm } from 'src/container/auth';
-// import GuestGuard from 'src/guard/GuestGuard';
+import GuestGuard from 'src/guard/GuestGuard.js';
 
 export default function ResetPassword() {
   return (
